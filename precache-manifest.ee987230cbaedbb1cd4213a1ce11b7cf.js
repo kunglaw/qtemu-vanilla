66111204a09000ff70223317a9b6bb5bb9f57332
kunglaw <alhusna901@gmail.com>
2020-12-13 00:14:58 +0700

    Updates

---
+++ precache-manifest.ee987230cbaedbb1cd4213a1ce11b7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d05d72596f81ad4b0532",
+    "url": "/qtemu-vanilla/static/js/runtime~main.d05d7259.js"
+  },
+  {
+    "revision": "55fcfb1168586b407929",
+    "url": "/qtemu-vanilla/static/js/main.55fcfb11.chunk.js"
+  },
+  {
+    "revision": "b5812664a0507d4c63e6",
+    "url": "/qtemu-vanilla/static/js/1.b5812664.chunk.js"
+  },
+  {
+    "revision": "55fcfb1168586b407929",
+    "url": "/qtemu-vanilla/static/css/main.c87c166a.chunk.css"
+  },
+  {
+    "revision": "5f680dfa084e5c2c2980e16d1560a236",
+    "url": "/qtemu-vanilla/index.html"
+  }
+];
